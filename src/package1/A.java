1d543b7fe66a672b72a83184447eb6bd213d8cbf
isaac <isaacolubusoye@gmail.com>
2023-12-20 17:39:26 +0100

    new to github

---
+++ src/package1/A.java
@@ -0,0 +1,17 @@
+package package1;
+import package2.*;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		B b = new B();
+//		System.out.println(b.privateMessage);
+		
+		// the above line of code won't work bcos private methods works inside the main class alone
+		
+	}
+	
+
+}
